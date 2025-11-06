1efcd196b7d9f9ba84f043e309512561db8521c2
barzelinger <bar.zelinger@post.runi.ac.il>
2025-11-06 23:07:43 +0200

    Update CalcPi.java

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,16 @@
 // Computes an approximation of PI.
+//  צריך לשים 5 רווחים בשורה השנייה של הפלט אחרי :approximated ורווח אחד בשורה הראשונה
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+		int N = Integer.parseInt(args[0]);
+		double x = 1.0;
+		double sum = 0.0;
+		// calculating PI by the formula.
+		for (int i = 0; i<= N-1; i++){
+			sum = sum + Math.pow(-1, i)/x;
+			x = x + 2;
+		}
+		sum = sum*4;
+		System.out.println("pi according to Java: " + Math.PI + "\npi, aproximated:      " + sum);
 	}
 }
