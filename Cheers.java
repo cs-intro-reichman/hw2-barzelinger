629ad9e69ef1a858aaa736760346d019140e7c48
barzelinger <bar.zelinger@post.runi.ac.il>
2025-11-06 23:07:20 +0200

    Update Cheers.java

--- Cheers.java
+++ Cheers.java
@@ -1,13 +1,34 @@
-//feedback
-
-/*
-Great work boaz!
-*/
-
-
 // Prints a crowd cheering output.
 public class Cheers {
-        public static void main(String[] args) {
-	    // Replace this comment with your code
+    public static void main(String[] args) {
+
+        String cheer = args[0].toUpperCase(); 
+        int count = Integer.parseInt(args[1]);
+        String add_AN = "RAEFHILMNOSX";
+        int num_of_chars = cheer.length(); // gets the amount of letters in the cheer.
+        String an_a = "";
+
+        // go over each letter from the cheer.
+        for (int i = 0; i < num_of_chars; i++){ 
+                char letter = cheer.charAt(i);
+                // go over each letter from the string.
+                for (int n = 0; n < add_AN.length(); n++){ 
+                        // check if letter in the add_AN string, if so then return an and break else return a
+                        if ((String.valueOf(letter)).indexOf(add_AN.charAt(n)) != -1){ 
+                                an_a = "an ";
+                        break;
+                        }
+                        else {
+                                an_a = "a  ";
+                        }
+                }
+                // print the first part of the cheer.
+                System.out.println("Give me " + an_a + " " + letter + ": " + letter + "!");
         }
+        // print the cheer the amount of times given.
+        for (int i = 0; i < count; i++){
+                System.out.println(cheer + "!!!");
+        }
+}
+
 }
